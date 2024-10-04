a71357e554621a8be0f8069e93964f225ee0f9ed
Robert Kuzet <robert@Roberts-MacBook-Air.local>
2024-10-04 14:11:23 +0200

    worked on the loop which displays all the info in the array

--- teams.php
+++ teams.php
@@ -5,6 +5,12 @@ require_once "data.php";
 print_r($teams['Barcelona']);
 
 echo "<h2>Barcelona Information:</h2>";
-foreach ($teams['Barcelona'] as $key => $value) {
-    echo $key, $value;
+foreach ($teams as $team => $teamInfo) {
+    echo $team . " ";
+    echo $teamInfo['league'] . " ";
+    echo $teamInfo['uefa-coefficient-ranking'] . " ";
+    echo $teamInfo['city'] . " ";
+    echo $teamInfo['url'] . " ";
+    echo $teamInfo['logo'] . " ";
+    echo $teamInfo['group'] . " ";
 }
